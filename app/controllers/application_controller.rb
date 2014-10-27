699e91a9f554272033c807778ee23482f2add47b
Anthony Ferragano <aferragano@gmail.com>
2014-10-27 16:32:45 -0400

    nav link_to. homepg. lint_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+    def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+    if @user
+      session[:id] = @user.id
+      redirect_to '/'
+    else
+      flash[:error] = "Login failed, please try again."
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+
   def current_user
     @user_session = User.find_by_id(session[:id])
   end
